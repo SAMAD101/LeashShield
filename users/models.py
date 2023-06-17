1ea80c825c45b2f36ee0695edb7d668a6c6dbdfc
Sam <asamadans@gmail.com>
2023-06-17 16:11:27 +0530

    custom users modals made

--- users/models.py
+++ users/models.py
@@ -1,7 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-class CustomUser(AbstractUser):
-    pass
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, username, email, masterpassword, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        if not masterpassword:
+            raise ValueError('The given masterpassword must be set')
+        
+        user = self.model(
+            username=username,
+            email=email,
+            masterpassword=masterpassword,
+            **extra_fields
+            )
+        user.set_password(masterpassword)
+        user.save()
+        return user
     
+    def create_user(self, username, email, masterpassword, **extra_fields):
+        extra_fields.setdefault('is_staff',False)
+        extra_fields.setdefault('is_superuser',False)
+        extra_fields.setdefault('is_active',True)
+        return self._create_user(username, email, masterpassword, **extra_fields)
+    
+    def create_superuser(self, username, email, masterpassword, **extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+        return self._create_user(username, email, masterpassword, **extra_fields)
+    
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    masterpassword = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
