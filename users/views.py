9e1cd2af8951c38ef68f01ad7f1fecc622f101e9
Sam <asamadans@gmail.com>
2023-06-18 14:49:25 +0530

    dong

--- users/views.py
+++ users/views.py
@@ -6,31 +6,29 @@ from django.contrib.auth import authenticate, login
 
 def LoginView(request):
     if request.method == 'POST':
-        username = request.POST.get['username']
-        emial = request.POST.get['email']
-        masterpassword = request.POST.get['masterpassword']
+        username = request.POST.get('username')
+        emial = request.POST.get('email')
+        masterpassword = request.POST.get('masterpassword')
                 
         user = authenticate(username=username, password=masterpassword)
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard/?user='+username)
-        else:
-            return render(request, 'login.html', {'error_message': 'Invalid login'})   
-    return render(request, 'users/login.html')
+        login(request, user)
+        return redirect('dashboard/?user='+username)
+    return redirect('redirect')
 
-def RegisterView(request):
-    if request.method == 'POST':
-        username = request.POST.get['username']
-        email = request.POST.get['email']
-        masterpassword = request.POST.get['masterpassword']
+def RegisterView(response):
+    if response.method == 'POST':
+        username = response.POST.get('username')
+        email = response.POST.get('email')
+        masterpassword = response.POST.get('masterpassword')
     
         if CustomUser.objects.filter(email=email).exists():
             return HttpResponse('User with this email already exist')
-    
+        if CustomUser.objects.filter(username=username).exists():
+            return HttpResponse('User with this username already exist')
         user = CustomUser.objects.create_user(username, email, masterpassword)
         user.save()
         return redirect('login')
-    return render(request, 'users/register.html')
+    return render(response, 'users/register.html')
 
 def DashboardView(request):
     passwords = Passwords.objects.filter(user=request.user)
