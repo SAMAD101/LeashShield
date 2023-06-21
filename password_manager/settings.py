6d5f3151520ddde30110fc32ed43fe818462680e
Sam <asamadans@gmail.com>
2023-06-21 18:11:48 +0530

    yeeted root installed npm packages, will just use cdn

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'home.apps.HomeConfig',
     'crispy_forms',
-    'compressor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'node_modules'),
 ]
 
---
+++ static/custom.js
@@ -0,0 +1,11 @@
+function showAlert() {
+  alert('Hello from JavaScript!');
+}
+
+// Event handler for a button click
+function handleButtonClick() {
+  alert('Button clicked!');
+}
+  
+
+
